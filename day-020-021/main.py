68f99c25fd2fa8357ebc9d6d237c387155347b3a
gamzekecibas <gkecibas16@ku.edu.tr>
2023-06-17 23:47:15 +0300

    Create food and scoreboard classes

--- day-020-021/main.py
+++ day-020-021/main.py
@@ -3,6 +3,9 @@
 from turtle import Turtle, Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +14,8 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -25,6 +30,10 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    ## Detect collusion with food
+    if snake.snake_head.distance(food) < 15:
+        food.refresh()
 
 ##  for part in snake_body:
 ##      part.forward(20)
