b9d73563d8a5c9fbdbc732605ac02fb941e22fe1
gamzekecibas <gkecibas16@ku.edu.tr>
2023-06-19 22:58:22 +0300

    Update walls boundary rules

--- day-020-021/main.py
+++ day-020-021/main.py
@@ -38,7 +38,7 @@ while game_is_on:
         scoreboard.increase_score()
 
     ## Detect collusion with wall
-    if (snake.snake_head.xcor() > 280 or snake.snake_head.xcor() < -280 or snake.snake_head.ycor() > 280 or snake.snake_head.ycor() < -280):
+    if (snake.snake_head.xcor() > 290 or snake.snake_head.xcor() < -290 or snake.snake_head.ycor() > 290 or snake.snake_head.ycor() < -290):
         game_is_on = False
         scoreboard.game_over()
 
