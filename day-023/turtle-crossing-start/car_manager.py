006e41c0d7131f599d2729eccbf261332502b7ce
gamzekecibas <gkecibas16@ku.edu.tr>
2023-06-20 23:31:06 +0300

    Complete turtle crossing project

--- day-023/turtle-crossing-start/car_manager.py
+++ day-023/turtle-crossing-start/car_manager.py
@@ -1,7 +1,30 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        rand_num = random.randint(1, 100)
+        if rand_num % 5 == 0:
+            car = Turtle("square")
+            car.shapesize(stretch_wid=1, stretch_len=2)
+            car.penup()
+            car.color(random.choice(COLORS))
+            rand_y = random.randint(-230, 230)
+            car.goto(300, rand_y)
+            self.all_cars.append(car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
--- day-023/turtle-crossing-start/main.py
+++ day-023/turtle-crossing-start/main.py
@@ -5,10 +5,33 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
+theTurtle = Player()
+manageCars = CarManager()
+scoreboard = Scoreboard()
+
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+screen.listen()
+screen.onkey(theTurtle.move_north, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    manageCars.create_car()
+    manageCars.move_cars()
+
+    ## Detect collusion turtle & cars
+    for car in manageCars.all_cars:
+        if car.distance(theTurtle) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    ## Detect to reach north edge
+    if theTurtle.ycor() > 280:
+        theTurtle.goto(0, -280)
+        manageCars.level_up()
+        scoreboard.update_level()
+screen.exitonclick()
