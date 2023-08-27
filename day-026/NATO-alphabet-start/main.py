457df3229b21c85e96783c1033a9aad8fa2ab7d3
gamzekecibas <gkecibas16@ku.edu.tr>
2023-08-27 20:20:42 +0300

    Add try-except block to handle non-letter inputs

--- day-026/NATO-alphabet-start/main.py
+++ day-026/NATO-alphabet-start/main.py
@@ -5,10 +5,17 @@ def spelling(alphabet_dict, word):
     spelled_word = [alphabet_dict[letter.upper()] for letter in word]
     return spelled_word
 '''
-#TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
 alphabet_df = pd.read_csv('nato_phonetic_alphabet.csv', sep=',')
 alphabet_dict = {alphabet_df.iloc[index, 0]:alphabet_df.iloc[index, 1] for (index, row) in alphabet_df.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_word = input('Enter a word: ')
-print([alphabet_dict[letter.upper()] for letter in user_word])
+def generate_phonetic():
+    user_word = input('Enter a word: ')
+    try:
+        nato_letters = [alphabet_dict[letter.upper()] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please!")
+        generate_phonetic()
+    else:
+        print(nato_letters)
+
+generate_phonetic()
